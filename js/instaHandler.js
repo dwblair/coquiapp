28c311c67fca8b2431501bd46b0085cc384b4cd2
Yuchen Su <psaux0@gmail.com>
2015-02-08 17:02:29 -0500

    photo

--- js/instaHandler.js
+++ js/instaHandler.js
@@ -1,4 +1,5 @@
 function runfeed(){
+    document.getElementById('instafeed').innerHTML='';
     tgName = "openwater" + document.getElementById("instaTag").value;
     var feed = new Instafeed({
 	get: 'tagged',
@@ -6,7 +7,7 @@ function runfeed(){
 	clientId: '1ed94341d26548b9b51bd42bfb0dc0e4',
 	resolution: 'low_resolution',
 	limit: 1,
-	template: '<a href="{{link}}"><img src="{{image}}" /></a>'
+	template: '<a href="{{link}}"><img src="{{image}}" /></a><r/><h3>location: {{location}}</h3><br/>'
     });
     feed.run();
 };
