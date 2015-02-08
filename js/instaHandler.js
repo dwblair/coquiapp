8274ae7c4a427ccda24cc2e68bce944682924eb9
Yuchen Su <psaux0@gmail.com>
2015-02-08 17:16:18 -0500

    photo

--- js/instaHandler.js
+++ js/instaHandler.js
@@ -7,7 +7,7 @@ function runfeed(){
 	clientId: '1ed94341d26548b9b51bd42bfb0dc0e4',
 	resolution: 'low_resolution',
 	limit: 1,
-	template: '<h3>Picture</h3><a href="{{link}}"><img src="{{image}}" /></a><br/>'
+	template: '<h3>Picture</h3><a href="{{link}}"><img src="{{image}}" /></a>'
     });
     feed.run();
 };
