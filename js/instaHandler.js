67d55ff9f50dd8608eb8199295b7760e8297c89f
Yuchen Su <psaux0@gmail.com>
2015-02-08 16:47:40 -0500

    update

--- js/instaHandler.js
+++ js/instaHandler.js
@@ -1,14 +1,12 @@
-var tgName = "openwaterproject";
-var feed = new Instafeed({
-    get: 'tagged',
-    tagName: tgName,
-    clientId: '1ed94341d26548b9b51bd42bfb0dc0e4',
-    resolution: 'low_resolution',
-    limit: 1,
-    template: '<a href="{{link}}"><img src="{{image}}" /></a>'
-});
-
 function runfeed(){
     tgName = "openwater" + document.getElementById("instaTag").value;
-    feed.run()
+    var feed = new Instafeed({
+	get: 'tagged',
+	tagName: tgName,
+	clientId: '1ed94341d26548b9b51bd42bfb0dc0e4',
+	resolution: 'low_resolution',
+	limit: 1,
+	template: '<a href="{{link}}"><img src="{{image}}" /></a>'
+    });
+    feed.run();
 };
