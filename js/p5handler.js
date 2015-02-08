e977d01f5bad79b57b8cd4b81f9cc325c91198d6
Don Blair <donblair@gmail.com>
2015-02-08 18:45:57 -0500

    update

--- js/p5handler.js
+++ js/p5handler.js
@@ -7,7 +7,7 @@ function setup(){
     var fftCanvas = createCanvas(710,400);
     fftCanvas.parent(document.getElementById('p5fft'));
 
-    specBar = createGraphics(100,400);
+    specBar = createGraphics(400,100);
     
     noFill();
     stroke(192, 57, 43);
@@ -23,7 +23,7 @@ function draw(){
     background(26, 188, 156);
     specBar.background(52, 73, 94);
     specBar.noStroke();
-    specBar.fill(211, 84, 0);
+    specBar.fill(255,255, 51);
 
     var spectrum = fft.analyze();
     var maxSpec = -1;
@@ -39,8 +39,9 @@ function draw(){
     }
     endShape();
 
-    specBar.rect(0,(255-maxFre)/255*height,100,maxFre/255*height);
-    image(specBar,610,0,100,400);
+    specBar.rect(map(maxFre,0,spectrum.length,0,500)+3,0,5,100);
+    image(specBar,0,150,710,10);
+    fill(255,0,0);
     document.getElementById('p5bar').innerHTML = "Dominant Frequency: " + maxFre;
 }
 
